cc23c8668f6305f0a4000ad378a6e0de96f63541
NgocLe-101 <ngocle266tqt@gmail.com>
2024-07-24 22:50:45 +0700

    Complete add book feature

--- main.js
+++ main.js
@@ -9,5 +9,61 @@ function Book(author, title, numPages) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    render();
 }
 
+function changeBookState(btn) {
+    console.log(btn);
+}
+
+function deleteBook(btn) {
+    console.log(btn);
+}
+
+function createCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    if (book._read === true) {
+        card.classList.add('read');
+    }
+    card.innerHTML = `
+        <div class="header">
+            <p class="state">${book._numPages}</p>
+        </div>
+        <div class="center">
+            <h1 class="title">${book._title}</h1>
+            <h3 class="author">${book._author}</h3>
+        </div>
+        <div class="footer">
+            <div class="btn-container">
+                <button class="btn" onclick="changeBookState(this)">Change</button>
+            </div>
+            <div class="btn-container">
+                <button class="btn" onclick="deleteBook(this)">Delete</button>
+            </div>
+        </div>
+    `;
+    return card;
+}
+
+function render() {
+    const cardBox = document.querySelector('.card-box');
+    cardBox.appendChild(createCard(myLibrary[myLibrary.length - 1]));
+}
+
+const inputField = document.querySelector('.book-input');
+inputField.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const author = inputField.querySelector('input#author').value;
+    const title = inputField.querySelector('input#title').value;
+    const numPages = inputField.querySelector('input#numPages').value;
+    addBookToLibrary(new Book(author, title, numPages));
+});
+
+function showForm() {
+    if (inputField.classList.contains('show')) {
+        inputField.classList.remove('show');
+        return;
+    }
+    inputField.classList.add('show');
+}
